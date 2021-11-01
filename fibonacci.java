cc55b50340a2a90ae51680a7e9ce083eb486b59a
Elenis Marios <71108397+iknowyourpassword@users.noreply.github.com>
2021-11-01 03:17:45 +0200

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -3,7 +3,6 @@ class fibonacci {
     static int fbsq(int n)
     {
   
-        
         int f[] = new int[n+2];
   
         int i;
@@ -13,12 +12,10 @@ class fibonacci {
   
         for (i=2; i <= n; i++) {
   
-           
-            f[i] = f[i-1] + f[i-2];
+           f[i] = f[i-1] + f[i-2];
         }
   
-       
-        return f[n];
+       return f[n];
     }
   
     public static void
