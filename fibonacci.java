8cf09a3afe3b204b28e5da298bb34f4cf206c2a9
Elenis Marios <71108397+iknowyourpassword@users.noreply.github.com>
2021-11-16 00:50:15 +0200

    Fbsq.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,31 +1,37 @@
 class fibonacci {
   
-    static int fbsq(int n)
+    static long fbsq(long n)
     {
   
-        int f[] = new int[n+2];
+        long f[] = new long[(int) (n+2)];
   
-        int i;
+        long i;
   
         f[0]=0;
         f[1]=1;
   
         for (i=2; i <= n; i++) {
   
-           f[i] = f[i-1] + f[i-2];
+           f[(int) i] = f[(int) (i-1)] + f[(int) (i-2)];
         }
   
-       return f[n];
+       return f[(int) n];
     }
   
     public static void
     main(String args[])
+   
     {
         
-        int N=23;
-  
-        for (int i=0; i<N; i++)
-            System.out.print(fbsq(i) + " ");
+        long N=86;
+        
+        for (long i=0; i<N; i++)
+          
+            System.out.printf("\n"+(fbsq(i) + " "));
+           
+
+
+            
     }
 }
 
